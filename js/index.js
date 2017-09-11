5ff6c2c3a708d2f6b38b14dcfacb068b45bb9042
gentlecoder <610249080@qq.com>
2017-09-11 19:18:30 +0800

    fix bugs

--- js/index.js
+++ js/index.js
@@ -2,7 +2,25 @@
  * Created by 10206072 on 2017/9/4.
  */
 $(function () {
-    var source = [{
+    var PATH = "http://10.47.200.129/iswareapm/",
+        source = [],
+        sourceTmp = [],
+        deviceTypes = [],
+        select = [];
+    // currentApp.appId
+    var topoParam = {};
+    // $.ajax({
+    //     url: PATH + "topo/toporealtion/" + currentApp.appId,
+    //     data: topoParam,
+    //     dataType: "json",
+    //     async: false,
+    //     success: function (result) {
+    //         if (result.data != null) {
+    //             source = JSON.parse(result.data.topoRelation);
+    //         }
+    //     }
+    // });
+    source = [{
         "id": "1",
         "deviceType": "出口交换机",
         "deviceName": "001",
@@ -57,7 +75,24 @@ $(function () {
         "deviceInIp": "1.1.1.1",
         "deviceOutIp": "1.10.101.102"
     }];
-    var deviceTypes = [
+    $.extend(true, sourceTmp, source);
+    var deviceTypesParam = {};
+    // $.ajax({
+    //     url: PATH + "topo/device",
+    //     data: deviceTypesParam,
+    //     dataType: "json",
+    //     async: false,
+    //     success: function (result) {
+    //         deviceTypes = result.data;
+    //         for (var i = 0; i < deviceTypes.length; i++) {
+    //             var tmp = {};
+    //             tmp.id = deviceTypes[i].id;
+    //             tmp.value = deviceTypes[i].deviceName;
+    //             select[i] = tmp;
+    //         }
+    //     }
+    // });
+    deviceTypes = [
         {
             "id": 1,
             "deviceName": "出口交换机",
@@ -94,7 +129,6 @@ $(function () {
             "deviceParams": "{\"名称\":\"name\",\"IP地址\":\"ip\",\"入端口\":\"inport\",\"出端口\":\"outport\"}"
         }
     ];
-    var select = [];
     for (var i = 0; i < deviceTypes.length; i++) {
         var tmp = {};
         tmp.id = deviceTypes[i].id;
@@ -112,105 +146,187 @@ $(function () {
             action: function (mode, data) {
                 console.dir(mode);
                 if (mode === 'save') {
+                    data._index = source.length;
                     source.push(data);
                 }
                 if (mode === 'edit') {
                     for (var i = 0; i < source.length; i++) {
-                        if (source[i].id == data.id) {
+                        if (source[i]._index == data._index) {
                             source[i] = data;
                         }
                     }
                 }
                 if (mode == 'delete') {
                     for (var i = 0; i < source.length; i++) {
-                        if (source[i].id == data) {
+                        if (source[i]._index == data) {
                             source.splice(i, 1);
                             break;
                         }
                     }
                 }
                 resetTabullet();
-                $('.sorted_table').sortable("destroy");
-                $('.sorted_table').sortable({
-                    containerSelector: 'table',
-                    itemPath: '> tbody',
-                    itemSelector: 'tr[data-tabullet-id!=-1]',
-                    placeholder: '<tr class="placeholder"/>',
-                    group: 'group',
-                    drag: 'false',
-                    onDragStart: function ($item, container, _super) {
-                        // Duplicate items of the no drop area
-                        if (!container.options.drop)
-                            $item.clone().insertAfter($item);
-                        _super($item, container);
-                    },
-                    onDrop: function ($item, container, _super, event) {
-                        $item.removeClass(container.group.options.draggedClass).removeAttr("style");
-                        $("body").removeClass(container.group.options.bodyClass);
-                        var sourceID = $item.children().eq(0).text();
-                        if ($item[0].nextElementSibling != null) {
-                            var targetID = $item[0].nextElementSibling.firstElementChild.textContent - 1;
-                        } else {
-                            var targetID = source.length - 1;
-                        }
-                        var tmp = source;
-                        source = [];
-                        for (var i = 0; i < tmp.length; i++) {
-                            if (i < sourceID || i > targetID) {
-                                source[i] = tmp[i]
-                            } else if (i >= sourceID && i < targetID) {
-                                source[i] = tmp[i + 1]
-                            } else if (i == targetID) {
-                                source[i] = tmp[sourceID]
-                            }
-                        }
-                    }
-                });
+                dissortableTable();
+                sortableTable();
             }
         });
     }
 
-    resetTabullet();
-
-    $('.sorted_table').sortable({
-        containerSelector: 'table',
-        itemPath: '> tbody',
-        itemSelector: 'tr[data-tabullet-id!=-1]',
-        handle: 'td[draggable]',
-        placeholder: '<tr class="placeholder"/>',
-        group: 'group',
-        drag: 'false',
-        onDragStart: function ($item, container, _super) {
-            // Duplicate items of the no drop area
-            if (!container.options.drop)
-                $item.clone().insertAfter($item);
-            _super($item, container);
-        },
-        onDrop: function ($item, container, _super, event) {
-            $item.removeClass(container.group.options.draggedClass).removeAttr("style");
-            $("body").removeClass(container.group.options.bodyClass);
-            var sourceID = $item.children().eq(0).text();
-            if ($item[0].nextElementSibling != null) {
-                var targetID = $item[0].nextElementSibling.firstElementChild.textContent - 1;
-            } else {
-                var targetID = source.length - 1;
-            }
-            var tmp = source;
-            source = [];
-            for (var i = 0; i < tmp.length; i++) {
-                if (i < sourceID || i > targetID) {
-                    source[i] = tmp[i]
-                } else if (i >= sourceID && i < targetID) {
-                    source[i] = tmp[i + 1]
-                } else if (i == targetID) {
-                    source[i] = tmp[sourceID]
+    function sortableTable() {
+        $('.sorted_table').sortable({
+            containerSelector: 'table',
+            itemPath: '> tbody',
+            itemSelector: 'tr[data-tabullet-id!=-1]',
+            handle: 'td[draggable]',
+            placeholder: '<tr class="placeholder"/>',
+            group: 'group',
+            drag: 'false',
+            onDragStart: function ($item, container, _super) {
+                // Duplicate items of the no drop area
+                if (!container.options.drop)
+                    $item.clone().insertAfter($item);
+                _super($item, container);
+            },
+            onDrop: function ($item, container, _super, event) {
+                $item.removeClass(container.group.options.draggedClass).removeAttr("style");
+                $("body").removeClass(container.group.options.bodyClass);
+                var sourceID = $item.children().eq(0).text();
+                if ($item[0].nextElementSibling != null) {
+                    var targetID = $item[0].nextElementSibling.firstElementChild.textContent;
+                } else {
+                    var targetID = source.length;
                 }
+                var tmp = source;
+                source = [];
+                if (sourceID > targetID) {
+                    for (var i = 0; i < tmp.length; i++) {
+                        if (i < targetID || i > sourceID) {
+                            source[i] = tmp[i]
+                        } else if (i > targetID && i <= sourceID) {
+                            source[i] = tmp[i - 1]
+                        } else if (i == targetID) {
+                            source[i] = tmp[sourceID]
+                        }
+                    }
+                } else {
+                    for (var i = 0; i < tmp.length; i++) {
+                        if (i < sourceID || i > targetID - 1) {
+                            source[i] = tmp[i]
+                        } else if (i >= sourceID && i < targetID - 1) {
+                            source[i] = tmp[i + 1]
+                        } else if (i == targetID - 1) {
+                            source[i] = tmp[sourceID]
+                        }
+                    }
+                }
+                resetTabullet();
+                dissortableTable();
+                sortableTable();
             }
-        }
-    });
-    // $( "tr[data-tabullet-id='-1']" ).sortable( {
+        });
+    }
+
+    function dissortableTable() {
+        $('.sorted_table').sortable("destroy");
+    }
+
+    resetTabullet();
+    sortableTable();
+
+
+    // $('.sorted_table').sortable({
+    //     containerSelector: 'table',
+    //     itemPath: '> tbody',
+    //     itemSelector: 'tr[data-tabullet-id!=-1]',
+    //     handle: 'td[draggable]',
+    //     placeholder: '<tr class="placeholder"/>',
     //     group: 'group',
-    //     // drop: false,
-    //     drag: false
-    // } );
+    //     drag: 'false',
+    //     onDragStart: function ($item, container, _super) {
+    //         // Duplicate items of the no drop area
+    //         if (!container.options.drop)
+    //             $item.clone().insertAfter($item);
+    //         _super($item, container);
+    //     },
+    //     onDrop: function ($item, container, _super, event) {
+    //         $item.removeClass(container.group.options.draggedClass).removeAttr("style");
+    //         $("body").removeClass(container.group.options.bodyClass);
+    //         var sourceID = $item.children().eq(0).text();
+    //         if ($item[0].nextElementSibling != null) {
+    //             var targetID = $item[0].nextElementSibling.firstElementChild.textContent;
+    //         } else {
+    //             var targetID = source.length;
+    //         }
+    //         var tmp = source;
+    //         source = [];
+    //         if (sourceID > targetID) {
+    //             for (var i = 0; i < tmp.length; i++) {
+    //                 if (i < targetID || i > sourceID) {
+    //                     source[i] = tmp[i]
+    //                 } else if (i > targetID && i <= sourceID) {
+    //                     source[i] = tmp[i - 1]
+    //                 } else if (i == targetID) {
+    //                     source[i] = tmp[sourceID]
+    //                 }
+    //             }
+    //         } else {
+    //             for (var i = 0; i < tmp.length; i++) {
+    //                 if (i < sourceID || i > targetID - 1) {
+    //                     source[i] = tmp[i]
+    //                 } else if (i >= sourceID && i < targetID - 1) {
+    //                     source[i] = tmp[i + 1]
+    //                 } else if (i == targetID - 1) {
+    //                     source[i] = tmp[sourceID]
+    //                 }
+    //             }
+    //         }
+    //         resetTabullet();
+    //     }
+    // });
+
+    $('#save').on('click', function () {
+        swal({
+            title: "成功!",
+            text: "应用拓扑配置成功!",
+            icon: "success",
+            button: "确定",
+        });
+        // var topoRelationParam = {};
+        // topoRelationParam.appId = currentApp.appId;
+        // // topoRelationParam.topoRelation = JSON.stringify(source).replace(/\[|]/g, '');
+        // topoRelationParam.topoRelation = JSON.stringify(source);
+        // $.ajax({
+        //     url: PATH + "topo/add",
+        //     data: JSON.stringify(topoRelationParam),
+        //     dataType: "json",
+        //     contentType: "application/json",
+        //     type: "POST",
+        //     success: function (result) {
+        //         swal({
+        //             title: "成功!",
+        //             text: "应用拓扑配置成功!",
+        //             icon: "success",
+        //             button: "确定",
+        //         });
+        //     },
+        //     error: function (result) {
+        //         swal({
+        //             title: "失败!",
+        //             text: "应用拓扑配置失败!",
+        //             icon: "warning",
+        //             button: "确定",
+        //         });
+        //     }
+        // });
+    })
+
+    $('#cancel').on('click', function () {
+        source = sourceTmp;
+        resetTabullet();
+        swal({
+            title: "成功!",
+            text: "应用拓扑配置重置成功!",
+            icon: "success",
+            button: "确定",
+        });
+    })
 });
